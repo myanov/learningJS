874b706a4fdc03f92d3182bf9292742c136abf87
myanov <m.yanov1998@gmail.com>
2019-05-14 02:51:42 +0700

    MoneyConverter

---
+++ MonyCourse/app.js
@@ -0,0 +1,25 @@
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
+
+inputRub.addEventListener('input', () => {
+    let request = new XMLHttpRequest();
+
+    // request.open(method, url, async, login, pass)
+    request.open('GET', 'current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    // status
+    // tatusText
+    // responseText / response
+    // readyState 
+
+    request.addEventListener('readystatechange', function() {
+        if(request.readyState === 4 && request.status == 200) {
+            let data = JSON.parse(request.response);
+            inputUsd.value = inputRub.value / data.course;
+        } else {
+            inputUsd.value = "Что-то пошло не так";
+        }
+    });
+});
