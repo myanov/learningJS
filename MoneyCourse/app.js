9f4ef793c411a8e40ef8fbf28943a8f26b0d5486
myanov <m.yanov1998@gmail.com>
2019-05-19 16:30:10 +0700

    Add silder

--- MoneyCourse/app.js
+++ MoneyCourse/app.js
@@ -1,25 +1,56 @@
-let inputRub = document.getElementById('rub'),
-    inputUsd = document.getElementById('usd');
+// let inputRub = document.getElementById('rub'),
+//     inputUsd = document.getElementById('usd');
 
-inputRub.addEventListener('input', () => {
-    let request = new XMLHttpRequest();
+// inputRub.addEventListener('input', () => {
+//     let request = new XMLHttpRequest();
 
-    // request.open(method, url, async, login, pass)
-    request.open('GET', 'current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    request.send();
+//     // request.open(method, url, async, login, pass)
+//     request.open('GET', 'current.json');
+//     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+//     request.send();
 
-    // status
-    // tatusText
-    // responseText / response
-    // readyState 
+//     // status
+//     // tatusText
+//     // responseText / response
+//     // readyState 
 
-    request.addEventListener('readystatechange', function() {
-        if(request.readyState === 4 && request.status == 200) {
-            let data = JSON.parse(request.response);
+//     request.addEventListener('readystatechange', function() {
+//         if(request.readyState === 4 && request.status == 200) {
+//             let data = JSON.parse(request.response);
+//             inputUsd.value = inputRub.value / data.course;
+//         } else {
+//             inputUsd.value = "Что-то пошло не так";
+//         }
+//     });
+// });
+
+let inputRub = document.querySelector('#rub'),
+    inputUsd = document.querySelector('#usd');
+
+inputRub.addEventListener('input', ()=> {
+    function catchMoney() {
+        return new Promise(function(resolve, reject) {
+            let request = new XMLHttpRequest();
+            request.open('GET', 'current.json');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            request.send();
+
+            request.onload = function() {
+                if(request.readyState === 4 && request.status == 200) {
+                    resolve(this.response);
+                } else {
+                    reject();
+                }
+            }
+        });
+    }
+
+    catchMoney()
+        .then(response => {
+            console.log(response);
+            let data = JSON.parse(response);
             inputUsd.value = inputRub.value / data.course;
-        } else {
-            inputUsd.value = "Что-то пошло не так";
-        }
-    });
+        })
+        .then(() => console.log(5555))
+        .catch(()=> inputUsd.value = 'Что-то пошло не так')
 });
--- Timer/js/script.js
+++ Timer/js/script.js
@@ -133,34 +133,38 @@ window.addEventListener('DOMContentLoaded', function() {
     form.addEventListener('submit', function(event) {
         event.preventDefault();
         form.appendChild(statusMessage);
-
-        let request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-
         let formData = new FormData(form);
 
-        let obj = {};
-        formData.forEach(function(value, key) {
-            obj[key] = value;
-        });
-        let json = JSON.stringify(obj);
+        function postData(data) {
+            return new Request(function(resolve, reject) {
+                let request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+                request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 
-        request.send(json);
-
-        request.addEventListener('readystatechange', function() {
-            if (request.readyState < 4) {
-                statusMessage.innerHTML = message.loading;
-            } else if(request.readyState === 4 && request.status == 200) {
-                statusMessage.innerHTML = message.success;
-            } else {
-                statusMessage.innerHTML = message.failure;
-            }
-        });
-
-        for (let i = 0; i < input.length; i++) {
-            input[i].value = '';
+                request.addEventListener('readystatechange', function() {
+                    if (request.readyState < 4) {
+                        resolve()
+                    } else if(request.readyState === 4 && request.status == 200) {
+                        resolve()
+                    } else {
+                        reject()
+                    }
+                });
+                request.send(data);
+            });
         }
+
+        function clearInput() {
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = '';
+            }
+        }
+
+        postData(formData)
+            .then(()=> statusMessage.innerHTML = message.loading)
+            .then(()=> statusMessage.innerHTML = message.success)
+            .catch(()=> statusMessage.innerHTML = message.failure)
+            .then(clearInput)
     });
 
     // contact form
@@ -200,4 +204,47 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrapper = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        slides.forEach(slide => slide.style.display = 'none');
+        dots.forEach(dot => dot.classList.remove('dot-active'));
+        slides[slideIndex-1].style.display = 'block';
+        dots[slideIndex-1].classList.add('dot-active');
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function() {
+        plusSlides(-1);
+    });
+    next.addEventListener('click', function() {
+        plusSlides(1);
+    });
+
+    dotsWrapper.addEventListener('click', function(event) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+                currentSlide(i);
+            }
+        }
+    })
 });
